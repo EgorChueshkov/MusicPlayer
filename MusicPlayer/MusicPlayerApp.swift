d0e8550149e4129d043e013a2c97e99645f3d3ce
EgorChueshkov <egor.chueshkov@yandex.by>
2024-08-11 22:09:42 +0300

    created project MusicPlayer

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Egor on 11.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
